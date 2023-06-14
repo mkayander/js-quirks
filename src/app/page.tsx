c0438e253e8aaba2e95b511111b62497489cec10
mkayander <maxim.kayander1@gmail.com>
2023-06-14 23:28:44 +0200

    refactor: Made reusable topic item component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,29 @@
 import Link from "next/link";
+import React from "react";
+import { UrlObject } from "url";
+
+type TopicItemProps = {
+  href: string | UrlObject;
+  title: string;
+  description: string;
+};
+
+const TopicItem: React.FC<TopicItemProps> = ({ href, title, description }) => {
+  return (
+    <Link
+      href={href}
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+    >
+      <h2 className="mb-3 text-2xl font-semibold">
+        {title}{" "}
+        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+          -&gt;
+        </span>
+      </h2>
+      <p className="m-0 max-w-[30ch] text-sm opacity-50">{description}</p>
+    </Link>
+  );
+};
 
 export default function Home() {
   return (
@@ -8,37 +33,17 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid gap-8 text-center sm:mb-0 sm:grid-cols-2 sm:text-left">
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Next.JS{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <TopicItem
+          href="https://nextjs.org/learn"
+          title="Next.JS"
+          description="Learn about Next.js in an interactive course with&nbsp;quizzes!"
+        />
 
-        <Link
+        <TopicItem
           href="/topic/hashing"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Hashing{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about hashing and its applications.
-          </p>
-        </Link>
+          title="Hashing"
+          description="Learn about hashing and its applications."
+        />
       </div>
     </div>
   );
