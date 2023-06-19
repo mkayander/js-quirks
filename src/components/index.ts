55561230836d4cf12d44210d1719da67b52e0b2d
mkayander <maxim.kayander1@gmail.com>
2023-06-19 23:33:14 +0200

    feat: Implemented Switch component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { CodePanel } from "./CodePanel";
 export { ResizeHandle } from "./ResizeHandle/ResizeHandle";
 export { SplitPanelsLayout } from "./SplitPanelsLayout";
+export { Switch } from "./Switch";
 export { TopicItem } from "./TopicItem";
