c342b3c9914135261c2b07abcd60f38b8cb0ad78
Max <maxim.kayander1@gmail.com>
2023-06-20 23:01:33 +0200

    feat: Added test 3D canvas

--- src/components/HashCircle/HashCircle3D.tsx
+++ src/components/HashCircle/HashCircle3D.tsx
@@ -1,24 +1,53 @@
-import { HashItem } from "@/components/HashCircle/HashItem";
-import React from "react";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useRef, useState } from "react";
+import { Mesh } from "three";
 import { HashCircleProps } from "./hashingTypes";
+
+const Box = (props: any) => {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef<Mesh>();
+  // Hold state for hovered and clicked events
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => {
+    if (!ref.current) return;
+    ref.current.rotation.x += delta;
+  });
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={isClicked ? 1.5 : 1}
+      onClick={(event) => setIsClicked(!isClicked)}
+      onPointerOver={(event) => setIsHovered(true)}
+      onPointerOut={(event) => setIsHovered(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={isHovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
 
 export const HashCircle3D: React.FC<HashCircleProps> = ({
   size = 428,
   items,
 }) => {
   return (
-    <div
-      style={{
-        width: size,
-        height: size,
-      }}
-      className="relative flex flex-col items-center justify-center rounded-full border-4 border-indigo-300 shadow-xl shadow-indigo-300/20"
-    >
-      <span>Hash Circle</span>
-
-      {items?.map(({ id, rotation }) => (
-        <HashItem key={id} rotation={rotation} />
-      ))}
+    <div className="h-[60vh] w-full">
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Box position={[-1.2, 0, 0]} />
+        {items?.map(({ id, rotation }) => (
+          <Box key={id} position={[1.2, rotation / 50 - 4, 0]} />
+        ))}
+        <OrbitControls />
+      </Canvas>
     </div>
   );
 };
