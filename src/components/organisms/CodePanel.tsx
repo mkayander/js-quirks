2f473c62d72c1aec585b09947c31f50e0c46f68c
mkayander <maxim.kayander1@gmail.com>
2023-09-12 00:36:33 +0300

    feat: Apply a passed-in code template

--- src/components/organisms/CodePanel.tsx
+++ src/components/organisms/CodePanel.tsx
@@ -2,39 +2,21 @@
 
 import { Editor } from "@monaco-editor/react";
 import { editor } from "monaco-editor";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import IStandaloneCodeEditor = editor.IStandaloneCodeEditor;
-
-const defaultCode = `import { useQuery } from "@trpc/react";
-import { ssr } from "client/trpc";
-
-export default function Home() {
-    const query = useQuery(["hello"]);
-    return (
-        <div>
-            <h1>hello world</h1>
-            <pre>{JSON.stringify(query.data, null, 4)}</pre>
-        </div>
-    );
-}
-
-export const getStaticProps = async () => {
-    const result = await ssr.query("hello");
-    return {
-        props: {
-            trpcState: ssr.getStaticPropsHelper(result),
-        },
-    };
-};
-`;
 
 type CodePanelProps = {
   title?: string;
+  codeTemplate?: string;
 };
 
-export const CodePanel: React.FC<CodePanelProps> = () => {
+export const CodePanel: React.FC<CodePanelProps> = ({ codeTemplate = "" }) => {
   const [editor, setEditor] = useState<IStandaloneCodeEditor | null>(null);
-  const [codeInput, setCodeInput] = useState(defaultCode);
+  const [codeInput, setCodeInput] = useState(codeTemplate);
+
+  useEffect(() => {
+    setCodeInput(codeTemplate);
+  }, [codeTemplate]);
 
   return (
     <div>
