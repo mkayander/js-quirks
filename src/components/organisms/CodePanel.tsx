c1e9fc911679823afc6741ae93f5838389492243
mkayander <maxim.kayander1@gmail.com>
2023-09-14 23:33:21 +0300

    feat: Implement basic code execution with custom logging

--- src/components/organisms/CodePanel.tsx
+++ src/components/organisms/CodePanel.tsx
@@ -1,9 +1,21 @@
 "use client";
 
+import { Button } from "@/components/atoms/Button";
 import { Editor } from "@monaco-editor/react";
 import { editor } from "monaco-editor";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import IStandaloneCodeEditor = editor.IStandaloneCodeEditor;
+
+const customLog = (...args: any[]) => {
+  console.log("custom log: ", ...args);
+};
+
+Object.assign(window, { customLog });
+
+const codePrefix = `
+const console = {...window.console, log: window.customLog};
+
+`;
 
 type CodePanelProps = {
   title?: string;
@@ -18,8 +30,23 @@ export const CodePanel: React.FC<CodePanelProps> = ({ codeTemplate = "" }) => {
     setCodeInput(codeTemplate);
   }, [codeTemplate]);
 
+  const handleRunCode = useCallback(() => {
+    try {
+      const prefixedCode = codePrefix + codeInput;
+      const code = new Function(prefixedCode);
+      console.time("runCode");
+      console.log("Code result: ", code());
+      console.timeEnd("runCode");
+    } catch (e) {
+      console.error(e);
+    }
+  }, [codeInput]);
+
   return (
-    <div>
+    <div className="flex flex-col gap-4">
+      <div className="mx-4 my-1 flex justify-end">
+        <Button onClick={handleRunCode}>Run ▶</Button>
+      </div>
       <Editor
         height="calc(100vh - 134px)"
         theme="vs-dark"
